645fa52e6eace90b9598a5786adadd9ddfd22723
Payton Swick <payton@foolord.com>
2020-01-22 18:36:36 -0500

    Update exec to pipe from stdin

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const exec = require('./utils').exec;
 
-exec('ls', { echo: true });
+(async function() {
+	const output = await exec('ls', ['-l'], { echoOutput: false });
+	await exec('rm', ['-i', 'testfile.js']);
+
+	console.log('output is', output);
+
+	process.exit(); // Necessary to stop the pipe from exec
+})();
--- utils.js
+++ utils.js
@@ -1,46 +1,24 @@
-const spawn = require('child_process').spawn;
+const { spawn } = require('child_process');
 
-/**
- * Executes shell command as it would happen in BASH script
- *
- * Copied from https://blog.cloudboost.io/node-js-writing-shell-scripts-using-modern-javascript-instead-of-bash-774e0859f965
- *
- * @param {string} command
- * @param {Object} [options] Object with options. Set `capture` to true, to capture and return stdout.
- *                           Set `echo` to true, to echo command passed.
- * @returns {Promise<{code: number, data: string | undefined, error: Object}>}
- */
-function exec(command, { capture = false, echo = false } = {}) {
-	if (echo) {
-		console.log(command);
-	}
-
-	const childProcess = spawn('bash', ['-c', command], {
-		stdio: capture ? 'pipe' : 'inherit',
-	});
-
-	return new Promise((resolve, reject) => {
-		let stdout = '';
-
-		if (capture) {
-			childProcess.stdout.on('data', data => {
-				stdout += data;
-			});
+function exec(command, args, { echoCommand = true, echoOutput = true } = {}) {
+	return new Promise(resolve => {
+		if (echoCommand) {
+			console.log(command, args.join(' '));
 		}
 
-		childProcess.on('error', function(error) {
-			reject({ code: 1, error: error });
-		});
+		let output = '';
 
-		childProcess.on('close', function(code) {
-			if (code > 0) {
-				reject({
-					code: code,
-					error: 'Command failed with code ' + code,
-				});
-			} else {
-				resolve({ code: code, data: stdout });
+		const child = spawn(command, args, {});
+		process.stdin.pipe(child.stdin);
+		child.stdout.on('data', data => {
+			output += data;
+			if (echoOutput) {
+				process.stdout.write(data);
 			}
+		});
+		child.stderr.pipe(process.stderr);
+		child.on('close', () => {
+			resolve(output);
 		});
 	});
 }
