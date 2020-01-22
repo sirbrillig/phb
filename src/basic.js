62048772ed3d87dd7d2bdf41c9aa59e9ed96d42c
Payton Swick <payton@foolord.com>
2020-01-22 16:58:40 -0500

    Add getActiveDiff

--- src/basic.js
+++ src/basic.js
@@ -3,9 +3,26 @@ function init(options = {}) {
 
 	const setActiveRevision = async id => options.writeRevision(id);
 
+	const getActiveDiff = async () => {
+		const revisionId = await getActiveRevision();
+		if (!revisionId) {
+			return null;
+		}
+		const diffId = await options.readDiff();
+		if (!diffId) {
+			return null;
+		}
+		const idsInRevision = await options.readAllDiffs(revisionId);
+		if (!idsInRevision.includes(diffId)) {
+			return null;
+		}
+		return diffId;
+	};
+
 	return {
 		getActiveRevision,
 		setActiveRevision,
+		getActiveDiff,
 	};
 }
 
--- test/basic.test.js
+++ test/basic.test.js
@@ -9,7 +9,7 @@ describe('getActiveRevision', () => {
 		expect(id).toBeNull();
 	});
 
-	it('returns the active id when a revision is saved', async () => {
+	it('returns the active revision id when a revision is saved', async () => {
 		const revisionId = 'D1234';
 		const { getActiveRevision } = init({
 			readRevision: async () => revisionId,
@@ -30,5 +30,53 @@ describe('setActiveRevision', () => {
 		await setActiveRevision(revisionId);
 		const id = await getActiveRevision();
 		expect(id).toBe(revisionId);
+	});
+});
+
+describe('getActiveDiff', () => {
+	it('returns null when no revision is saved', async () => {
+		const { getActiveDiff } = init({
+			readRevision: async () => null,
+			readDiff: async () => '321',
+			readAllDiffs: async () => ['111', '222', '444'],
+		});
+		const id = await getActiveDiff();
+		expect(id).toBeNull();
+	});
+
+	it('returns null when a revision is saved but the diff id is not in the revision', async () => {
+		const revisionId = 'D1234';
+		const { getActiveDiff } = init({
+			readRevision: async () => revisionId,
+			readDiff: async () => '321',
+			readAllDiffs: async revision =>
+				revision === revisionId ? ['111', '222', '444'] : ['443'],
+		});
+		const id = await getActiveDiff();
+		expect(id).toBeNull();
+	});
+
+	it('returns null when a revision is saved but no diff id is saved', async () => {
+		const revisionId = 'D1234';
+		const { getActiveDiff } = init({
+			readRevision: async () => revisionId,
+			readDiff: async () => null,
+			readAllDiffs: async revision =>
+				revision === revisionId ? ['111', '222', '444'] : ['443'],
+		});
+		const id = await getActiveDiff();
+		expect(id).toBeNull();
+	});
+
+	it('returns the diff id when a revision is saved and the diff id is in the revision', async () => {
+		const revisionId = 'D1234';
+		const { getActiveDiff } = init({
+			readRevision: async () => revisionId,
+			readDiff: async () => '222',
+			readAllDiffs: async revision =>
+				revision === revisionId ? ['111', '222', '444'] : ['443'],
+		});
+		const id = await getActiveDiff();
+		expect(id).toBe('222');
 	});
 });
