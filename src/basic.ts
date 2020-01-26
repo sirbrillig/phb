f22d87dc84816d203ab77237036d0ab37b66971f
Payton Swick <payton@foolord.com>
2020-01-26 17:17:31 -0500

    Convert basic to TS

--- src/basic.ts
+++ src/basic.ts
@@ -1,37 +1,54 @@
-const noop = async () => null;
-const defaultOptions = {
-	readRevision: noop,
-	writeRevision: noop,
-	readDiff: noop,
-	writeDiff: noop,
-	readAllDiffs: noop,
-	runArcCommand: noop,
-	runArcConduitCommand: noop,
-};
+interface FileInterface {
+	readRevision: () => Promise<string>;
+	writeRevision: (data: string) => null;
+	readDiff: () => Promise<string>;
+	writeDiff: (data: string) => null;
+	readAllDiffs: (revisionId: string) => Promise<string>;
+}
 
-function init(options) {
-	options = { ...defaultOptions, ...options };
-	const getActiveRevision = async () => options.readRevision();
+interface ArcInterface {
+	runArcCommand: (command: string) => Promise<string>;
+	runArcConduitCommand: (command: string, data: object) => Promise<ArcConduitCommandResult>;
+}
 
-	const setActiveRevision = async id => options.writeRevision(id);
+interface ArcConduitCommandResult {
+	errorMessage?: string;
+	response?: Array<ArcConduitResponse>;
+}
+
+interface ArcConduitResponse {
+	diffs?: Array<string>;
+}
+
+function init({
+	fileInterface,
+	arcInterface,
+}: {
+	fileInterface: FileInterface;
+	arcInterface: ArcInterface;
+}) {
+	const getActiveRevision = async () => fileInterface.readRevision();
+
+	const setActiveRevision = async (id: string) =>
+		fileInterface.writeRevision(id);
 
 	const getActiveDiff = async () => {
 		const revisionId = await getActiveRevision();
 		if (!revisionId) {
 			return null;
 		}
-		const diffId = await options.readDiff();
+		const diffId = await fileInterface.readDiff();
 		if (!diffId) {
 			return null;
 		}
-		const idsInRevision = await options.readAllDiffs(revisionId);
+		const idsInRevision = await fileInterface.readAllDiffs(revisionId);
 		if (!idsInRevision.includes(diffId)) {
 			return null;
 		}
 		return diffId;
 	};
 
-	const setActiveDiff = async id => options.writeDiff(id);
+	const setActiveDiff = async (id: string) => fileInterface.writeDiff(id);
 
 	const createNewRevision = async () => {
 		// TODO: verify we are in root directory
@@ -40,7 +57,7 @@ function init(options) {
 		// TODO: Verify that all files are in the same svn repo
 		// TODO: open editor to get commit message (and use unused previously written message if it exists)
 		// TODO: handle errors and allow retrying
-		const output = await options.runArcCommand('diff --create');
+		const output = await arcInterface.runArcCommand('diff --create');
 		if (!output) {
 			throw new Error('Arc command did not return anything');
 		}
@@ -49,11 +66,14 @@ function init(options) {
 			throw new Error('Could not find revision id in arc command output');
 		}
 		const revisionId = results[1];
-		options.writeRevision(revisionId);
+		fileInterface.writeRevision(revisionId);
 
-		const jsonData = options.runArcConduitCommand('differential.query', {
-			ids: [stripDFromRevisionId(revisionId)],
-		});
+		const jsonData = await arcInterface.runArcConduitCommand(
+			'differential.query',
+			{
+				ids: [stripDFromRevisionId(revisionId)],
+			}
+		);
 		if (jsonData && jsonData.errorMessage) {
 			throw new Error(
 				`Revision created but an error occurred trying to find differential ids in revision "${revisionId}": ${jsonData.errorMessage}`
@@ -75,7 +95,7 @@ function init(options) {
 				`Revision created but could not find differential ids in revision "${revisionId}"`
 			);
 		}
-		options.writeDiff(diffIds[diffIds.length - 1]);
+		fileInterface.writeDiff(diffIds[diffIds.length - 1]);
 	};
 
 	return {
@@ -87,7 +107,7 @@ function init(options) {
 	};
 }
 
-function stripDFromRevisionId(revisionId) {
+function stripDFromRevisionId(revisionId: string): string {
 	return revisionId.substr(1);
 }
 
